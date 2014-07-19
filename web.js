b08adddfb8478b1cfd1864af7a6f8f85d28b26a3
rahul-raghavan <rahul.glass@gmail.com>
2014-07-19 06:59:08 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,14 +1,28 @@
 
 var express = require('express');
 var logger = require('morgan');
+var fs = require('fs');
 
 var app = express();
+var txt;
+
+fs.readFile('./index.html','utf-8', function(err,data) {
+        if (err){
+            return console.log(err);
+        }
+    console.log("Read file");
+    txt = data.toString('utf-8');
+    });
 
 app.get('/', function(request, response){
-    response.send('Hello World 2');
+  
+  response.send(txt); 
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
-    console.log("Listening on " + port);
+    console.log("I am Listening on " + port);
 });
+
+
+
